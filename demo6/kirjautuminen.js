e493766512c4f97bcda26efa4227252a8c038f13
timolfmn <timo.lofman@edu.keuda.fi>
2022-03-07 12:44:30 +0200

    demo6 + 7

---
+++ demo6/kirjautuminen.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut(){
+let kirjautunut='kylla';
+if(kirjautunut==='kylla'){
+    document.getElementById("tervetulo_teksti").textContent +=' Timo!';
+}
+}
