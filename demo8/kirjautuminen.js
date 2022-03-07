54031b2db194cec84cc53cb5382a119af0ab770b
timolfmn <timo.lofman@edu.keuda.fi>
2022-03-07 13:05:23 +0200

    demo8

---
+++ demo8/kirjautuminen.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", onkoKirjautunut);
+
+function onkoKirjautunut(){
+    if (localStorage.getItem("kirjautunut") === "kylla"){
+        document.getElementById("tervetulo_teksti").textContent += ' ' + localStorage.getItem("nimi");
+        document.getElementById("kirjautumis_lomake").style.display = "none";
+    }
+    else {
+        document.getElementById("uloskirj").style.display = "none";
+    }
+}
+
+function kirjaudu(){
+    localStorage.setItem("nimi", document.getElementById("nimi").value);
+    localStorage.setItem("kirjautunut", "kylla");
+}
+function ulos() {
+    localStorage.clear();
+}
